d0037a1c4d13572042c4c30b411e82b3f715c9b6
Kim Fransson <kim.fransson.22@gmail.com>
2025-10-19 22:24:48 +0200

    Small max width fix

--- src/components/GetInTouch/GetInTouch.jsx
+++ src/components/GetInTouch/GetInTouch.jsx
@@ -64,7 +64,7 @@ const TextWrapper = styled.div`
 `;
 
 const TitleWrapper = styled.div`
-  max-width: ${268 / 16}rem;
+  max-width: ${300 / 16}rem;
 
   @media ${QUERIES.tabletAndSmaller} {
     max-width: ${335 / 16}rem;
